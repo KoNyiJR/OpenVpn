ad9d1b25a6df331a7252f20fda0babf79f4f2f04
HP <thegame.mzh@gmail.com>
2020-07-10 12:43:51 +0500

    Initial commit

--- app/src/main/java/com/open/vpn/MainActivity.java
+++ app/src/main/java/com/open/vpn/MainActivity.java
@@ -1,14 +1,165 @@
 package com.open.vpn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.VpnService;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import de.blinkt.openvpn.OpenVpnApi;
+import de.blinkt.openvpn.core.OpenVPNService;
+import de.blinkt.openvpn.core.OpenVPNThread;
 
 public class MainActivity extends AppCompatActivity {
+    private OpenVPNThread vpnThread = new OpenVPNThread();
+    private OpenVPNService vpnService = new OpenVPNService();
+    boolean vpnStart = false;
+    private SharedPreference preference;
+    private Server server;
+    private static final String TAG = "Test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preference = new SharedPreference(this);
+        server = preference.getServer();
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, new IntentFilter("connectionState"));
+
     }
+
+    public void connectServer(View view) {
+        if (vpnStart) {
+            Log.d("ok", "ok");
+        } else {
+            prepareVpn();
+        }
+    }
+
+    private void prepareVpn() {
+        if (!vpnStart) {
+
+            // Checking permission for network monitor
+            Intent intent = VpnService.prepare(this);
+
+            if (intent != null) {
+                startActivityForResult(intent, 1);
+            } else startVpn();//have already permission
+
+            // Update confection status
+            Log.d(TAG, "prepareVpn: connecting");
+
+
+        } else if (stopVpn()) {
+            Log.d(TAG, "prepareVpn: Disconnect Successfully");
+            // VPN is stopped, show a Toast message.
+        }
+    }
+
+    /**
+     * Stop vpn
+     *
+     * @return boolean: VPN status
+     */
+    public boolean stopVpn() {
+        try {
+            vpnThread.stop();
+            Log.d(TAG, "stopVpn: connect");
+            vpnStart = false;
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    /**
+     * Taking permission for network access
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            //Permission granted, start the VPN
+            startVpn();
+        } else {
+            Log.d(TAG, "onActivityResult: Permission Deny !!");
+        }
+    }
+
+    /**
+     * Start the VPN
+     */
+    private void startVpn() {
+        try {
+            // .ovpn file
+            InputStream conf = getAssets().open(server.getOvpn());
+            InputStreamReader isr = new InputStreamReader(conf);
+            BufferedReader br = new BufferedReader(isr);
+            String config = "";
+            String line;
+
+            while (true) {
+                line = br.readLine();
+                if (line == null) break;
+                config += line + "\n";
+            }
+
+            br.readLine();
+            OpenVpnApi.startVpn(this, config, server.getOvpnUserName(), server.getOvpnUserPassword());
+
+            // Update log
+            Log.d(TAG, "startVpn: Connecting ");
+
+            vpnStart = true;
+
+        } catch (IOException | RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Receive broadcast message
+     */
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                Log.d(TAG, "onReceive: "+intent.getStringExtra("state"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+
+                String duration = intent.getStringExtra("duration");
+                String lastPacketReceive = intent.getStringExtra("lastPacketReceive");
+                String byteIn = intent.getStringExtra("byteIn");
+                String byteOut = intent.getStringExtra("byteOut");
+
+                if (duration.equals(null)) duration = "00:00:00";
+                if (lastPacketReceive.equals(null)) lastPacketReceive = "0";
+                if (byteIn.equals(null)) byteIn = " ";
+                if (byteOut.equals(null)) byteOut = " ";
+                Log.d(TAG, "onReceive: "+duration+" "+ lastPacketReceive+" "+ byteIn+" "+ byteOut);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
 }
